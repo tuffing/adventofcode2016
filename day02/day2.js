5547b70266bce4ecf7d64357f57ca0d6918e40dd
tuffing <logan.orr@tuffing.co.nz>
2019-03-07 21:45:36 +1300

    removed unneeded nested for loop

--- day02/day2.js
+++ day02/day2.js
@@ -5,48 +5,42 @@ class DayTwo {
 	Process(values, dial, x, y) {
 		let result = '';
 
-		for (let val of values) {
-			for (let l of val) {
-				let oldX = x;
-				let oldY = y;
-				
-				if (l === 'U') {
-					y--;
-				}
-				else if (l === 'D') {
-					y++;
-				}
-				else if (l === 'L') {
-					x--;
-				}
-				else {
-					x++;
-				}
-
-				if (y < 0 || y >= dial.length || x < 0 || x >= dial.length || dial[y][x] === '') {
-					x = oldX;
-					y = oldY;
-				}
+		for (let l of values) {
+			let oldX = x;
+			let oldY = y;
+			
+			if (l === 'U') {
+				y--;
+			}
+			else if (l === 'D') {
+				y++;
+			}
+			else if (l === 'L') {
+				x--;
+			}
+			else if (l === 'R') {
+				x++;
+			}
+			else {
+				result += dial[y][x];
 			}
 
-			result += dial[y][x];
+			if (dial[y] == undefined || dial[y][x] == undefined || dial[y][x] === '') {
+				x = oldX;
+				y = oldY;
+			}
 		}
 
-		return result;
+		return result += dial[y][x];
 	}
 
-	PartASolution(input) {
-		let values = input.split(' ');
-
+	PartASolution(values) {
 		let dial = [['1','2','3'],['4','5','6'],['7','8','9']];
 
 		return this.Process(values, dial, 1, 1);
 	}
 
-	PartBSolution(input) {
-		let values = input.split(' ');
-		let result = '';
-
+	PartBSolution(values) {
 		let dial = [['','','1', '',''],['','2','3','4',''],['5','6','7','8', '9'],['','A','B','C',''], ['','','D','','']];
 
 		return this.Process(values, dial, 0, 2);
