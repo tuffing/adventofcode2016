58b130a244684f012793217ea139a6988335a3b4
tuffing <logan.orr@tuffing.co.nz>
2019-03-07 21:27:12 +1300

    removed duplication.

--- day02/day2.js
+++ day02/day2.js
@@ -2,49 +2,8 @@ class DayTwo {
 	constructor() {
 	}
 
-	PartASolution(input) {
-		let values = input.split(' ');
+	Process(values, dial, x, y) {
 		let result = '';
-
-		let dial = [['1','2','3'],['4','5','6'],['7','8','9']];
-		let x = 1;
-		let y = 1;
-
-		for (let val of values) {
-			for (let l of val) {
-				//ULL RRDDD
-				switch (l) {
-					case 'U':  //UP
-						if (y > 0)
-							y--;
-						break;
-					case 'D': //DOWN
-						if (y < 2)
-							y++;
-						break;
-					case 'L': //LEFT
-						if (x > 0)
-							x--;
-						break;
-					default: //RIGHT
-						if (x < 2)
-							x++;
-				}
-			}
-
-			result += dial[y][x];
-		}
-
-		return result;
-	}
-
-	PartBSolution(input) {
-		let values = input.split(' ');
-		let result = '';
-
-		let dial = [['','','1', '',''],['','2','3','4',''],['5','6','7','8', '9'],['','A','B','C',''], ['','','D','','']];
-		let x = 0;
-		let y = 2;
 
 		for (let val of values) {
 			for (let l of val) {
@@ -74,5 +33,22 @@ class DayTwo {
 		}
 
 		return result;
+	}
+
+	PartASolution(input) {
+		let values = input.split(' ');
+
+		let dial = [['1','2','3'],['4','5','6'],['7','8','9']];
+
+		return this.Process(values, dial, 1, 1);
+	}
+
+	PartBSolution(input) {
+		let values = input.split(' ');
+		let result = '';
+
+		let dial = [['','','1', '',''],['','2','3','4',''],['5','6','7','8', '9'],['','A','B','C',''], ['','','D','','']];
+
+		return this.Process(values, dial, 0, 2);
 	}
 }
