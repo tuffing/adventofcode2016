f813192756e2aa5f2b2cbc18173b7d87295e50e0
tuffing <logan.orr@tuffing.co.nz>
2019-03-12 23:37:24 +1300

    day 7 part2

--- day07/day7.js
+++ day07/day7.js
@@ -5,15 +5,24 @@ class DaySeven {
 	IsValidTLS(text) {
 			return (text.search(/(.)(.)(?!\1\1)\2\1(?=\w*\])/) === -1 && text.search(/(.)(.)(?!\1\1)\2\1(?=\w*(\[|))/) >= 0);
 	}
+
+	IsValidSSL(text) {
+		return text.search(/(?:\[.*(.)(.)\1\w*\].*\2\1\2(?!\w*\]))|(?:(.)(.)\3(?!\w*\]).*(\[\w*\4\3\4))/) >= 0;
+	}
 	
 	Solution(inputText) {
-		let count = 0;
+		let countTSL = 0;
+		let countSSL = 0;
 		for (let str of inputText.split('\n')) {
 			if (this.IsValidTLS(str)) {
-				count++;
+				countTSL++;
+			}
+
+			if (this.IsValidSSL(str)) {
+				countSSL++;
 			}
 		}
 
-		return count;
+		return [countTSL, countSSL];
 	}
 }
