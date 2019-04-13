c5842695a26ebad0ebc664bf96f41d1bad54ed8f
tuffing <logan.orr@tuffing.co.nz>
2019-04-14 11:36:14 +1200

    Day 23

---
+++ day23/day23.js
@@ -0,0 +1,123 @@
+class DayTwentyThree {
+	constructor() {
+	}
+
+	Solution(inputText, regc = 0, rega = 0, regb = 0) {
+		let lines = inputText.split('\n');
+
+		let registers = {'a' : rega, 'b' : regb, 'c' : regc, 'd' : 0, 'e' : 0, 'ins': 0, instructions: []};
+		let instructions = registers.instructions;
+
+		lines.forEach(l => {
+			instructions.push(l.split(' '));
+		});
+
+		while (registers['ins'] >= 0 && registers['ins'] < instructions.length) {
+			this[instructions[registers['ins']][0]](registers, ...(instructions[registers['ins']].slice(1)));
+		}
+		return registers['a'];
+	}
+
+	Solution2(rega, multiA, multiB) {
+		// Our entire program can be broke down into the following short program:
+		// the factorial of rega + the result of 2 numbers (in our program 73 and 80)
+	 	let x = 1;
+	 	for (let i = rega; i > 1; i--) {
+	 		x *= i;
+	 	}
+
+	 	return x + (multiA * multiB);
+	}
+
+	/*
+		cpy x y copies x (either an integer or the value of a register) into register y.
+	*/
+	cpy(registers, x, y) {
+		if (isNaN(+x)) {
+			x = registers[x];
+		}
+
+		if (isNaN(+y)) {
+			registers[y] = +x;
+		}
+		registers['ins']++;
+	}
+
+	/*
+		inc x increases the value of register x by one.
+	*/
+	inc(registers, x) {
+		registers[x]++;
+		registers['ins']++;
+	} 
+
+	/*
+		dec x decreases the value of register x by one.
+	*/
+	dec(registers, x) {
+		registers[x]--;
+		registers['ins']++
+	}
+
+	/*
+		jnz x y jumps to an instruction y away (positive means forward; negative means backward), but only if x is not zero.
+	*/
+	jnz(registers, x, y) {
+		if (isNaN(+x)) {
+			x = registers[x];
+		}
+
+		if (isNaN(+y)) {
+			y = registers[y];
+		}
+
+		if (+x !== 0) {
+			registers['ins'] += +y;
+		}
+		else {
+			registers['ins']++;
+		}
+	}
+
+	/*tgl x toggles the instruction x away (pointing at instructions like jnz does: positive means forward; negative means backward):
+
+	For one-argument instructions, inc becomes dec, and all other one-argument instructions become inc.
+	For two-argument instructions, jnz becomes cpy, and all other two-instructions become jnz.
+	The arguments of a toggled instruction are not affected.
+	If an attempt is made to toggle an instruction outside the program, nothing happens.
+	If toggling produces an invalid instruction (like cpy 1 2) and an attempt is later made to execute that instruction, skip it instead.
+	If tgl toggles itself (for example, if a is 0, tgl a would target itself and become inc a), the resulting instruction is not executed
+	 until the next time it is reached.
+	*/
+	tgl (registers, x) {
+		if (isNaN(+x)) {
+			x = registers[x];
+		}
+
+		let ins = registers['ins'] + +x;
+		registers['ins']++;
+
+		if (ins < 0 || ins >= registers['instructions'].length)
+			return;
+
+		let com = registers['instructions'][ins][0];
+		let newCom = '';
+
+		switch (com) {
+			case 'inc':
+				newCom = 'dec'
+				break;
+			case 'dec':
+			case 'tgl':
+				newCom = 'inc';
+				break;
+			case 'jnz':
+				newCom = 'cpy';
+				break;
+			default:
+				newCom = 'jnz';
+		}
+
+		registers['instructions'][ins][0] = newCom;
+	}
+}
